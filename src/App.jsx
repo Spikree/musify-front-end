80d91a1cd8bb9fa37216f3c85a572b1172fe9710
Avi <avimahalingpure10@gmail.com>
2024-06-29 00:16:07 +0530

    box shadow

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Sidebar from './components/sidebar/Sidebar'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/Home/Home'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div style={{margin:0}} className='app'>
       <Navbar/>
       <Sidebar className="sidebar-main"/>
-      <Home/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+        </Routes>
     </div>
   )
 }
